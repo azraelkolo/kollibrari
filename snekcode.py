ca01e286c550684a70e07af46b8bde22b5bb04e6
Wade Diann <161585671+azraelkolo@users.noreply.github.com>
2024-02-28 12:20:28 +0200

    Create snekcode.py

---
+++ snekcode.py
@@ -0,0 +1,28 @@
+def install(package):
+    """
+    Automatically installs python pip package and restarts the program
+    """
+    try:
+        pip.main(["install",  package])
+    except AttributeError:
+        check_call([sys.executable, '-m', 'pip', 'install', package])
+
+    execl(sys.executable, sys.executable, *sys.argv)
+
+try:
+    import cpuinfo
+except ModuleNotFoundError:
+    print("Cpuinfo is not installed. "
+          + "Miner will try to automatically install it "
+          + "If it fails, please manually execute "
+          + "python3 -m pip install py-cpuinfo")
+    install("py-cpuinfo")
+
+try:    
+    import psutil   
+except ModuleNotFoundError: 
+    print("Psutil is not installed. "   
+          + "Miner will try to automatically install it "   
+          + "If it fails, please manually execute " 
+          + "python3 -m pip install psutil")    
+    install("psutil"
